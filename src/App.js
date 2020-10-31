9a6927d552e698226172c033d3d1a888673a206a
Mai Thien Quang <thienquang2804@gmail.com>
2020-10-31 11:25:50 +0700

    Final login layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import "./App.css";
 import Login from "./component/login";
+import Header from "./component/header";
 import React from "react";
-import { CssBaseline, makeStyles } from "@material-ui/core";
-
-const useStyles = makeStyles({
-  root: {
-    height: "100%",
-    width: "100%",
-  },
-});
 
 const App = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <Login />
-    </div>
-  );
+  return <Login />;
 };
 
 export default App;
--- src/component/login.js
+++ src/component/login.js
@@ -3,30 +3,45 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   root: {
     heigth: "100%",
+    display: "flex",
+    justifyContent: "center",
   },
   container: {
     height: "100vh",
     display: "flex",
     flexWrap: "no-wrap",
-    width: "60%",
-    margin: "0 auto",
+    width: "80%",
   },
+
   login: {
     display: "flex",
-    flexDirection: "column",
-    width: "40%",
-    justifyContent: "center",
     alignItems: "center",
-  },
-  slider: {
+    flexDirection: "column",
     width: "60%",
   },
   banner: {
+    margin: "auto 0",
     fontFamily: "Itim",
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    border: "1px solid red",
+    backgroundColor: "smoking-white",
+  },
+  logo: {
+    margin: "auto",
+  },
+  botName: {
+    marginBottom: "50px",
+    fontSize: "3rem",
+  },
+  loginButton: {
+    fontSize: "2.5rem",
+  },
+  slider: {
+    display: "flex",
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 }));
 
@@ -37,19 +52,24 @@ export default function Login() {
     <div className={classes.root}>
       <div className={classes.container}>
         <div className={classes.login}>
-          <div className={classes.banner}>
+          <div className={`${classes.content} ${classes.banner}`}>
             <img
+              className={classes.logo}
               src={"/assets/chatbot.svg"}
               alt="logo"
-              style={{ height: "auto", width: "80%" }}
+              style={{ height: "auto", width: "100%" }}
             ></img>
-            <h1>COMIC NOTIFY</h1>
-            <h2>Something likes slogan here</h2>
-            <button>Login with facebook</button>
+            <div className={classes.botName}>COMIC NOTIFY BOT</div>
+            <button className={classes.loginButton}>Login with facebook</button>
           </div>
         </div>
-        <div className={classes.slider}>
-          <div>Slider here</div>
+        <div className={(classes.content, classes.slider)}>
+          <img
+            className={classes.align}
+            src={"/assets/bot-logo.jpg"}
+            alt="slider"
+            style={{ height: "auto", width: "80%" }}
+          />
         </div>
       </div>
     </div>
